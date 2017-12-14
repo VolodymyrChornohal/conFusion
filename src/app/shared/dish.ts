2ba31c7f527da3832f9580eeb26a2168312ee40e
VolodymyrChornohal <vova.chornohal@gmail.com>
2017-12-15 00:00:55 +0200

    Components Part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Vladimir on 12/14/17.
+ */
+export class Dish{
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+}
