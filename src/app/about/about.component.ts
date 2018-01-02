06e6132d97df247458e3c630ab411d1927547d8d
VolodymyrChornohal <vova.chornohal@gmail.com>
2018-01-02 12:34:58 +0200

    RxJs Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -17,7 +17,7 @@ export class AboutComponent implements OnInit {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.leaderservice.getLeaders().then(leaders => this.leaders = leaders);
+    this.leaderservice.getLeaders().subscribe(leaders => this.leaders = leaders);
   }
 
 }
